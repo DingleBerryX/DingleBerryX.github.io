f3f9d485db1b3960606457c589b667b670137026
yuri <bau@kanv.me>
2025-04-14 17:32:41 -0500

    Update xsx.lua

--- xsx.lua
+++ xsx.lua
@@ -39,8 +39,7 @@ local library = {
 	BlurSize = 24,
 	FieldOfView = CurrentCam.FieldOfView,
 
-	-- Key = UserInputService.TouchEnabled and Enum.KeyCode.RightShift or Enum.KeyCode.Insert, -- OLD, doesn't work for everyone
-	Key = Enum.KeyCode.RightShift or Enum.KeyCode.Insert, -- new, should work
+	Key = {Enum.KeyCode.RightShift, Enum.KeyCode.Insert},
 	fps = 0,
 	Debug = true,
 
